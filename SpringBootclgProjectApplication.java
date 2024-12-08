0cc786a9fe210104bf7ef392929b4b0a9b377e7e
ashokreddy1408 <158316010+ashokreddy1408@users.noreply.github.com>
2024-12-08 08:39:16 +0530

    Add files via upload

---
+++ SpringBootclgProjectApplication.java
@@ -0,0 +1,14 @@
+package com.klef.jfsd.springbootclg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootclgProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootclgProjectApplication.class, args);
+	}
+
+}
+ 
